073a29dc29e7ddc7f59d3811a4aa267b41a24c2f
alifiogans <58583263+alifiogans@users.noreply.github.com>
2020-01-01 02:19:56 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+pkg install update && pkg install upgrade
+pkg install curl
+pkg install irssi
+pkg install bash
+pkg install figlet
+pkg install gem
+gem install lolcat
+pkg install git
+git clone https://github.com/alifiogans/AutoMetasploit
+git clone https://github.com/muhammadfathul/VIRTEX
+gem install lolcat
+pip2 install mechanize
+chmod +x AmbyarrCOK.sh
+sh AmbyarrCOK.sh
